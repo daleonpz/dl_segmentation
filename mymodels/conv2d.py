54b826201f246bd8172a1359f8f321bc7daf2c4a
daniel.paredes <daleonpz@gmail.com>
2022-12-23 17:43:19 +0100

    fix conv when up

--- mymodels/conv2d.py
+++ mymodels/conv2d.py
@@ -19,11 +19,11 @@ class Conv2d(torch.nn.Module):
         ### START CODE HERE ### (approx. 23 lines)
         if up:
             if pooling:
-                kernel, padding = 3, 1
+                kernel_size, padding = 3, 1
             else:
-                kernel, padding = 2, 0
+                kernel_size, padding = 2, 0
 
-            self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel, stride=1, padding=padding, bias=bias)
+            self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding, bias=bias)
         elif down:
             if bilinear:
                 self.conv = torch.nn.Sequential(
