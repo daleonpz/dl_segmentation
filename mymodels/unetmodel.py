3a130bb33ca23c0e5e063d4b81f31ee6d96f65b9
daniel.paredes <daleonpz@gmail.com>
2023-01-02 17:40:39 +0100

    debug

--- mymodels/unetmodel.py
+++ mymodels/unetmodel.py
@@ -101,11 +101,11 @@ class double_conv(nn.Module):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
-#             nn.BatchNorm2d(out_ch),
-#             nn.ReLU(inplace=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-#             nn.BatchNorm2d(out_ch),
-#             nn.ReLU(inplace=True)
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
